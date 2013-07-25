78ad3cbc4f39ac4ceb00daf5831579bbffd55e9e
Tobias Hoeppner <thoeppner@thoeppner.de>
2013-07-25 13:40:32 +0200

    [add] Gui.hs

---
+++ code/cnwgol/src/Gui.hs
@@ -0,0 +1,23 @@
+-----------------------------------------------------------------------------
+--
+-- Module      :  Gui
+-- Copyright   :
+-- License     :  AllRightsReserved
+--
+-- Maintainer  :
+-- Stability   :
+-- Portability :
+--
+-- |
+--
+-----------------------------------------------------------------------------
+
+module Gui (
+world
+) where
+
+import Graphics.Gloss.Interface.Pure.Simulate (..)
+
+world =
+
+
